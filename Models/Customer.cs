64877ae12ef86e4adf38f24d29496fc1023d02ca
Joshua Anaya <joshua.anaya@gmail.com>
2020-10-14 19:48:23 -0600

    Section 3 - Exercises 1 and 2
    I meant to split this into 2 commits, but oh well.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -13,6 +13,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
