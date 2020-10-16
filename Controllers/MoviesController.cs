1a6e25a13605adf11e8e4406216ec4c6e6e177aa
Joshua Anaya <joshua.anaya@gmail.com>
2020-10-15 19:09:07 -0600

    Section 5 - Exercise
    Used up all my dopamine in my banzai run yesterday. Also had a few meetings today that carved into my productivity. Took all day to grind through section 5. Wtf. T_T 
    
    Section 6 will go faster. I promise.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -34,8 +34,18 @@ namespace Vidly.Controllers
 
         // Create / Update
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
@@ -82,9 +92,8 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
